145510b2496521e756402de1d59fe92cee804e6d
P-Seongbeom <psb0929@gmail.com>
2022-06-21 17:34:54 +0900

    Add Class, Static Mesh Setting, Using UPROPERTY()
    클래스 추가 방식, 오브젝트, 액터, 폰, 캐릭터 상관 관계
    
    클래스 오브젝트 툴에서 추가 시 고정적인 Mesh 생성하기
    
    UPROPERTY() 리플렉션 이용해서 툴에서 다루기
    -UPROPERTY(VisibleAnywhere)
    -UPROPERTY(EditAnywhere)

---
+++ Source/UnrealTest/MyActor.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//CreateDefault를 사용해서 UStaticMeshComponent의 메모리를 자동적으로 관리
+	//Mesh 밑에 있는 Hp, Mp는 관리 안됨
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+
+	RootComponent = Mesh;
+
+	//MyActor를 여러번 뿌려도 로딩은 한번만 해도 되니까 static
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> StaticMesh(TEXT("StaticMesh'/Game/StarterContent/Props/SM_Couch.SM_Couch'"));
+
+	//StaticMesh 경로에서 성공적으로 찾으면 Mesh에다가 StaticMesh를 덮어 씌워줌
+	//코드상에서 먼저 세팅한 Mesh가 기본적으로 나온다.
+	//후에 툴에서 변경하면 툴에서 변경한 대로 바뀐다.
+	if (StaticMesh.Succeeded())
+	{
+		Mesh->SetStaticMesh(StaticMesh.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UnrealTest/MyActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class UNREALTEST_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)//툴에서 노출시키기
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, Category = BattleStat)//툴에서 수치 변경 가능
+	int32 Hp;
+
+	UPROPERTY(EditAnywhere, Category = BattleStat)
+	int32 Mp;
+};
